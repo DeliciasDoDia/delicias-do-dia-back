b45b729c7df1e03fcc1ac3f9f6bd7d0ed9faf6aa
PedroHMFernandes <pedrovosvisk10@gmail.com>
2025-09-21 20:28:28 -0300

    fix: cors

--- portal-receitas/src/main/java/com/mackenzie/receitas/portal_receitas/config/WebConfigure.java
+++ portal-receitas/src/main/java/com/mackenzie/receitas/portal_receitas/config/WebConfigure.java
@@ -12,7 +12,7 @@ public class WebConfigure implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:8080", "http://localhost:3000", "http://3.80.199.222:8080/", "http://ec2-3-80-199-222.compute-1.amazonaws.com:8080/")
+                .allowedOriginPatterns("*") // permite todas, compatível com credentials
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
