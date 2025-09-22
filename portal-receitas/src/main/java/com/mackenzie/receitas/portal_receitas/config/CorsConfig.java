23c7ba9bd0336597948f5f7182c3fc0ef6ad2e28
PedroHMFernandes <pedrovosvisk10@gmail.com>
2025-09-22 20:26:48 -0300

    test: Cors 4

--- portal-receitas/src/main/java/com/mackenzie/receitas/portal_receitas/config/CorsConfig.java
+++ portal-receitas/src/main/java/com/mackenzie/receitas/portal_receitas/config/CorsConfig.java
@@ -2,12 +2,10 @@ package com.mackenzie.receitas.portal_receitas.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfigure implements WebMvcConfigurer {
-
+public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
@@ -15,4 +13,3 @@ public class WebConfigure implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
-
