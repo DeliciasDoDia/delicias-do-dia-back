5cd7cae2fa5d1ea959b3e7b92a8078b50e090810
Pedro Fernandes <134298800+PedroHMFernandes@users.noreply.github.com>
2025-11-08 19:22:46 +0000

    test cors

--- portal-receitas/src/main/java/com/mackenzie/receitas/portal_receitas/config/CorsConfig.java
+++ portal-receitas/src/main/java/com/mackenzie/receitas/portal_receitas/config/CorsConfig.java
@@ -10,6 +10,8 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
                 .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE");
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(false);
     }
 }
